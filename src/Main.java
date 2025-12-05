84a7b0070a12dc5e62261e27fc22cf1c83a6bb0c
jordonjones <jordon01jones@gmail.com>
2025-12-05 13:27:06 -0600

    constant5

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
-//
+// Create constants for file paths,
+// database connection strings (use placeholder values),
+// and application version number.
 
 public class Main {
+    public static final String FILE_PATH1 = "E:\\downloads\\books\\";
+    public static final String FILE_PATH2 = "user\\jordon\\documents\\code";
+    public static final String DATABASE_CONNECTION = "jdbc:mysql://localhost:3306/mydatabase?user=myuser&password=mypass" ;
+    public static final String VERSION_NUMBER = "3.1.23" ;
+
     public static void main (String [] args){
 
     }
